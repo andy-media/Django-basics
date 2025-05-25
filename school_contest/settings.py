a9a5b9858894b8a602aa4b7845ad5abc2f44bb96
Andrews Antwi <atosandy2022@gmail.com>
2025-05-25 09:02:34 +0000

    modified database settings

--- school_contest/settings.py
+++ school_contest/settings.py
@@ -44,22 +44,22 @@ CSRF_TRUSTED_ORIGINS = ["https://django-basics-production.up.railway.app/"]
 
     # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+if os.getenv('ENVIRONMENT') == 'production':
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=os.getenv('DATABASE_URL'),
+            conn_max_age=600,
+            ssl_require=True,  # Enforce SSL (required for Render)
+        )
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL'),
-        conn_max_age=600,
-        ssl_require=True,  # Enforce SSL (required for Render)
-    )
-
-}
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+    } 
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 CLOUDINARY_STORAGE = {
     'CLOUDINARY_URL': os.getenv('CLOUDINARY_URL')
